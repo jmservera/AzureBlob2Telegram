aa584084adbb9ea8b9fe7dda758b0bde705f9f4b
Juan Manuel Servera Bondroit <juanserv@microsoft.com>
2020-04-20 00:59:21 +0200

    use piped input

--- video2telegramfunction/FromBlob/__init__.py
+++ video2telegramfunction/FromBlob/__init__.py
@@ -56,38 +56,33 @@ def main(inputBlob: func.InputStream):
 
     _, fullfilename = os.path.split(inputBlob.name)
     fullfilename = os.path.join(basepath, fullfilename)
-    reducedfilename, extension = os.path.splitext(fullfilename)
-    reducedfilename = os.path.join(basepath, f'{reducedfilename}_b{extension}')
-
-    # write the blob stream to a file
-    with open(fullfilename, "wb") as download_file:
-        while True:
-            buf = inputBlob.read(4194304)
-            download_file.write(bytes(buf))
-            if len(buf) == 0:
-                break
 
     cmd_out = ['ffmpeg',
                '-y',
-               '-i', fullfilename,
+               '-i', '-',  # use pipe
                audio,
-               '-vf', f'fps={fps},scale={width}:-1',  # todo, scale from config
-               '-c:v', 'h264',  # '-preset:v', 'ultrafast',
-               reducedfilename]
+               '-vf', f'fps={fps},scale={width}:-1',
+               '-c:v', 'h264',
+               fullfilename]
 
-    pipe = sp.Popen(cmd_out)
+    pipe = sp.Popen(cmd_out, stdin=sp.PIPE)
+
+    # write the blob stream to a file
+    while True:
+        buf = inputBlob.read(4194304)
+        pipe.stdin.write(buf)
+        if len(buf) == 0:
+            break
+    pipe.stdin.close()
 
     pipe.wait()
 
-    os.path.getsize(reducedfilename)
-    logging.info(f'File reduced:\n' +
-                 '\t{fullfilename}\t{os.path.getsize(fullfilename)}\n' +
-                 '\t{reducedfilename}\t{os.path.getsize(reducedfilename)}')
-    os.remove(fullfilename)
+    os.path.getsize(fullfilename)
+    logging.info(f'File reduced:\t{os.path.getsize(fullfilename)}')
 
     # send the reduced file to telegram
 
-    with open(reducedfilename, 'rb') as animation:
+    with open(fullfilename, 'rb') as animation:
         bot = telegram.Bot(telegram_token)
         if remove_audio:
             bot.send_animation(
@@ -96,21 +91,4 @@ def main(inputBlob: func.InputStream):
         else:
             bot.send_video(telegram_chat, animation)
 
-    os.remove(reducedfilename)
-    # cmd_out = ['ffmpeg',
-    #         '-y',  # (optional) overwrite output file if it exists
-    #         '-f', 'image2pipe',
-    #         '-vcodec', 'h264',
-    #         '-i', '-',  # The input comes from a pipe
-    #         '-vcodec', 'gif',
-    #         '-f','image2pipe',
-    #         '-o','-']
-    # pipe = sp.Popen(cmd_out, stdin=sp.PIPE)
-    # output, _ = pipe.communicate()
-
-    # # for screenshot in screenshot_list:
-    # #     im = Image.open(BytesIO(base64.b64decode(screenshot)))
-    # #     im.save(pipe.stdin, 'PNG')
-
-    # pipe.stdin.close()
-    # pipe.wait()
+    os.remove(fullfilename)
